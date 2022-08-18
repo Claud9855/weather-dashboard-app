6b7fb77e885029da532529fe43fc098a53191ae3
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-08-18 00:33:04 -0400

    added moment js to display current time and combined both api calls into one function and displaying current weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,62 @@
-// $(document).ready(function() {
-
-// });
-
-
 var apiKey = "b137ba82c0ac5142d6480bc2e0ad3380";
-var params;
 
-
-function getApi(){
-    getParam();
-    console.log(params);
-    if(params === null || params === undefined){
-        return;
-    }
-    var resquestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + params[0] + "&lon=" + params[1] + "&exclude=alerts&appid=" + apiKey;
-
-    setupLocalHistory(params);
-
-    fetch(resquestURL)
-    .then(function(response){
-        if (response.status === 200) {
-            console.log("ok");
-        }
-        else{
-            console.log("not ok");
-        }
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    });
-}
-
-function getParam(){
+var index = 0;
+async function getData(){
     var searchInput = $('#search');
     if(searchInput.val() === ""){
         alert("empty input. please enter a city name.");
         return null;
     }
     var url = "http://api.openweathermap.org/geo/1.0/direct?q="+ searchInput.val() + "&limit=1&appid=" + apiKey;
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        console.log(data[0].lat + ", " + data[0].lon + ", " + data[0].name);
-        params = [data[0].lat, data[0].lon, data[0].name];
-    });
+    searchInput.val("");
+
+    var response1 = await fetch(url);
+    var data1 = await response1.json();
+    var arr = [data1[0].lat, data1[0].lon, data1[0].name];
+    setupLocalHistory(arr);
+
+    var resquestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + $('#localhistory').children().eq(index).attr('data-latitude') + "&lon=" + $('#localhistory').children().eq(index).attr('data-longitude') + "&exclude=alerts&units=imperial&appid=" + apiKey;
+    index++;
+
+    // var responeses = await Promise.all([fetch(url),fetch(resquestURL)]);
+    var response2 = await fetch(resquestURL);
+    var data2 = await response2.json();
+    console.log(data2);
+    
+    var weatherData = [data2.current.temp, data2.current.wind_speed, data2.current.humidity, data2.current.uvi, $('button').eq(index).attr('data-name'), data2.current.weather[0].icon];
+    setupJumbotron(weatherData);
 }
+
 function setupLocalHistory(params){
-    var buttonGroupEl = $('#localHistory');
+    var buttonGroupEl = $('#localhistory');
     var buttonEl = $('<button>');
     buttonEl.attr('type', 'button');
-    buttonEl.attr('class', 'btn btn-primary');
+    buttonEl.attr('class', 'btn btn-primary form-control mb-3');
     buttonEl.attr('data-latitude', params[0]);
     buttonEl.attr('data-longitude', params[1]);
+    buttonEl.attr('data-name', params[2]);
     buttonEl.text(params[2]);
     buttonGroupEl.append(buttonEl);
 
 }
 
-$('#search-btn').on('click', getApi);
+function setupJumbotron(params){
+    var headingEl = $('#search-input');
+    var tempEl = $('#temp');
+    var windEl = $('#wind');
+    var humidityEl = $('#humidity');
+    var uvIndex = $('#uv-index');
+    headingEl.html(params[4] + " " + moment().format('(M/D/YYYY)') + "<span><img src='http://openweathermap.org/img/wn/" + params[5] + "@2x.png'/></span");
+    tempEl.html(params[0] + " &#8457");
+    windEl.text(params[1] + " MPH");
+    humidityEl.text(params[2] + " %");
+    uvIndex.text(params[3]);
+
+
+}
+
+function setupForecasts(){
+
+}
+
+$('#search-btn').on('click', getData);
